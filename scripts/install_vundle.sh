c2a7cf082769cc7ac2ac97d318e56a89319acc21
Hiroshi Chiyokawa <hiroshi.chiyokawa@gmail.com>
2014-04-03 00:04:01 +0900

    Fix vimrc (wip)

--- scripts/install_vundle.sh
+++ scripts/install_vundle.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Install Vundle
-VUNDLE_DIR=~/.vim/vundle.git
-if [ -d ${VUNDLE_DIR} ]; then
+NEOVUNDLE_DIR=~/.vim/neobundle.vim
+if [ -d ${NEOVUNDLE_DIR} ]; then
   echo Notice: Vundler has already installed on your system.
 else
-  git clone http://github.com/gmarik/vundle.git ${VUNDLE_DIR}
+  git clone http://github.com/gmarik/vundle.git ${NEOVUNDLE_DIR}
   
-  if [ -d ${VUNDLE_DIR} ]; then
-    echo "run ':BundleInstall' on your vim"
+  if [ -d ${NEOVUNDLE_DIR} ]; then
+    echo "run ':NeoBundleInstall' on your vim"
   else
-    echo Error: Retry install Vundler
+    echo Error: Retry install NeoBundle
   fi
 fi
