3873c7b42f0ec8f4d51a0c61ae113a58a2efec49
Hiroshi Chiyokawa <hiroshi.chiyokawa@gmail.com>
2014-10-10 18:52:29 +0900

    Add NeoBundle update feature and fix error message.

--- scripts/install_neobundle.sh
+++ scripts/install_neobundle.sh
@@ -4,12 +4,17 @@
 NEOVUNDLE_DIR=~/.vim/bundle/neobundle.vim
 if [ -d ${NEOVUNDLE_DIR} ]; then
   echo Notice: NeoBundle has already installed on your system.
+
+  # Update NeoBundle
+  cd $NEOVUNDLE_DIR
+  git pull --rebase origin master
+  cd
 else
   git clone https://github.com/Shougo/neobundle.vim ${NEOVUNDLE_DIR}
-  
+
   if [ -d ${NEOVUNDLE_DIR} ]; then
     echo "Info: run ':NeoBundleInstall' on your vim"
   else
-    echo Error: Retry install NeoBundle
+    echo Error: Cannot clone. Retry install NeoBundle.
   fi
 fi
