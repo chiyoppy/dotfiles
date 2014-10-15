0c3e950631b0d667cb51c23a1ada196acfd74291
Hiroshi Chiyokawa <hiroshi.chiyokawa@gmail.com>
2014-10-15 17:12:17 +0900

    Add rc file remover.

---
+++ scripts/remove_dotfiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo 'Removing rc files, which is linked to .dotfiles'
+echo 'Notice: Local rc files (like `.*_local`) will not be removed.'
+for f in `ls -a ~ | grep "^\."`; do
+  if readlink ~/$f > /dev/null; then
+    rm ~/$f
+  fi
+done
