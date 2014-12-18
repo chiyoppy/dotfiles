65077bbdbc756a7c4f451a80bd56621c723147b3
Hiroshi Chiyokawa <hiroshi.chiyokawa@gmail.com>
2014-12-18 20:21:58 +0900

    Fix message.

--- scripts/install_neobundle.sh
+++ scripts/install_neobundle.sh
@@ -3,7 +3,7 @@
 # Install Vundle
 NEOVUNDLE_DIR=~/.vim/bundle/neobundle.vim
 if [ -d ${NEOVUNDLE_DIR} ]; then
-  echo Notice: NeoBundle has already installed on your system.
+  echo [Notice] NeoBundle has already installed on your system.
 
   # Update NeoBundle
   cd $NEOVUNDLE_DIR
@@ -13,8 +13,8 @@ else
   git clone https://github.com/Shougo/neobundle.vim ${NEOVUNDLE_DIR}
 
   if [ -d ${NEOVUNDLE_DIR} ]; then
-    echo "Info: run ':NeoBundleInstall' on your vim"
+    echo [Info] run :NeoBundleInstall on your vim
   else
-    echo Error: Cannot clone. Retry install NeoBundle.
+    echo [Error] NeoBundle could not be cloned. Check the internet connection and retry.
   fi
 fi
