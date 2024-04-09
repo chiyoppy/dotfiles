0cca3d592d1e3a2a60b737eff8b1ec1663f7ea4b
Hiroshi Chiyokawa <hiroshi.chiyokawa@gmail.com>
2024-04-09 16:15:30 +0900

    Cleanup vimrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,7 @@
 #/bin/zsh
 
-echo "NOTE: install dein.vim manually https://github.com/Shougo/dein-installer.vim"
-
 # Link rc-files
 ./scripts/link_rc_files.sh
-
-# Install Vundle
-./scripts/install_dein_vim.sh
 
 # Generate gitconfig_local
 ./scripts/generate_gitconfig_local.sh
--- scripts/install_dein_vim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DEIN_VIM_INSTALLER_PATH=/tmp/dein_vim_installer.sh
-
-curl -Ss https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ${DEIN_VIM_INSTALLER_PATH}
-sh ${DEIN_VIM_INSTALLER_PATH} ~/.cache/dein
